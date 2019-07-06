61e105412e2c9dbbda92ead6f1f27dd103cd79fe
san0suke <sistemas.robson@gmail.com>
2019-07-06 13:54:02 -0300

    Powershell Project

---
+++ PowerShellProject/Reconstrucao.ps1
@@ -0,0 +1,12 @@
+﻿$hFilePath = "C:\Users\Kleber\Documents\projetos\claudia-faculdade\Imagem\H-1.txt"
+$gFilePath = "C:\Users\Kleber\Documents\projetos\claudia-faculdade\Imagem\g-1.txt"
+$testeFilePath = "C:\Users\Kleber\Documents\projetos\claudia-faculdade\Imagem\teste.txt"
+
+[System.Collections.ArrayList]$arrValuesG=@{}
+foreach($line in [System.IO.File]::ReadLines($hFilePath)) {
+    $elements=$line.split(",")
+    $arrValuesG.Add($elements) | Out-Null
+}
+
+Write-Output $arrValuesG[1000][300]
+Write-Output "Tudo certo"
